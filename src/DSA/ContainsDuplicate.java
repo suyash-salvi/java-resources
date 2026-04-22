99b8e45bc7bb9aa16d18aaa2952530b3ff4d5a19
suyash-salvi <suyashmessi123@gmail.com>
2026-04-23 01:14:07 +0530

    Contains duplicate solution

---
+++ src/DSA/ContainsDuplicate.java
@@ -0,0 +1,28 @@
+package DSA;
+
+import java.util.*;
+
+public class ContainsDuplicate {
+
+    public static void main(String[] args) {
+        ContainsDuplicate obj = new ContainsDuplicate();
+
+        // Test cases
+        int[] nums1 = {1, 2, 3, 1};
+        int[] nums2 = {1, 2, 3, 4};
+        int[] nums3 = {1, 1, 1, 3, 3, 4, 3, 2, 4, 2};
+
+        System.out.println(obj.containsDuplicate(nums1)); // expected: true
+        System.out.println(obj.containsDuplicate(nums2)); // expected: false
+        System.out.println(obj.containsDuplicate(nums3)); // expected: true
+    }
+
+    public boolean containsDuplicate(int[] nums) {
+        HashSet<Integer> setOfArray = new HashSet<>();
+        for (int num: nums) {
+            setOfArray.add(num);
+        }
+        return setOfArray.size() != nums.length;
+
+    }
+}
